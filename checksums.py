0decb743e780e41c4b230210d15292539f8debeb
Philip Rolfe <philip.rolfe@icloud.com>
2023-01-28 16:20:17 -0500

    second commit: merged files

--- checksums.py
+++ checksums.py
@@ -13,6 +13,16 @@ def write_list_to_file(my_list, filename):
 # filename = 'my_list.txt'
 # write_list_to_file(my_list, filename)
 
+def read_file_to_list(filename):
+    with open(filename, 'r') as f:
+        lines = f.read().splitlines()
+    return lines
+
+# Example usage
+# sum_log = 'log.txt'
+# log_list = read_file_to_list(sum_log)
+# print(log_list)
+
 def checksum(file_path):
     sha256 = hashlib.sha256()
     with open(file_path, 'rb') as f:
@@ -33,6 +43,25 @@ def checksum_directory(directory):
             write_list_to_file(stored_checksums, 'log.txt')
 path = input("Enter the path to the directory (/path/to/directory): ")
 checksum_directory(path)
+
+
+def verify_checksum(checksum, checksum_array):
+    if checksum in checksum_array:
+        print(f"Checksum: {checksum} \tMatch Found:\t{checksum}")
+        return True
+    else:
+        print(f"Checksum: {checksum} \tMatch Found:\tFalse")
+        return False
+
+
+def compare_lists(list1, list2):
+    for checksum in list1:
+        if checksum in list2:
+            print("Match found!")
+            return True
+    print("No match found.")
+    return False
+
 
 
 
